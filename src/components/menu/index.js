956b5ab012108d958759ca963a52743529ad1441
Lucas Henrique Paes de Carvalho <lukas.paes18@gmail.com>
2021-09-16 20:12:47 -0300

    chore: add modal

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,30 +1,37 @@
 import React from "react";
 import ButtonOne from "../atomic/ButtonOne";
-import * as S from './styled'
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+import * as S from "./styled";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Menu = () => {
-    return (
-        <S.Container>
+  return (
+    <S.Container>
+      <div>
+        <S.Strong href="#">
+          <strong>Doutor</strong>Reparo.com
+        </S.Strong>
+        <S.Paragraph>Mestre da informática</S.Paragraph>
+      </div>
 
-            <div>
-                <S.Strong href='#'><strong>Doutor</strong>Reparo.com</S.Strong>
-                <S.Paragraph>Mestre da informática</S.Paragraph>
-            </div>
-
-            <S.Navigation>
-                <AnchorLink href="#Service"  > <S.Link>NOSSOS SERVIÇOS</S.Link></AnchorLink>
-                <AnchorLink  href="#Garantia"><S.Link>NOSSA GARANTIA</S.Link></AnchorLink>
-                <AnchorLink href="#Story"> <S.Link >QUEM SOMOS</S.Link></AnchorLink>
-                <AnchorLink href="#Contact"> <S.Link  >CONTATO</S.Link></AnchorLink>
-                 <AnchorLink href="#Contact"><ButtonOne /></AnchorLink>
-            </S.Navigation>
-
-        </S.Container>
-    )
-
-
-  
+      <S.Navigation>
+        <AnchorLink href="#Service">
+          <S.Link>NOSSOS SERVIÇOS</S.Link>
+        </AnchorLink>
+        <AnchorLink href="#Garantia">
+          <S.Link>NOSSA GARANTIA</S.Link>
+        </AnchorLink>
+        <AnchorLink href="#Story">
+          <S.Link>QUEM SOMOS</S.Link>
+        </AnchorLink>
+        <AnchorLink href="#Contact">
+          <S.Link>CONTATO</S.Link>
+        </AnchorLink>
+        <AnchorLink href="#Contact">
+          <ButtonOne />
+        </AnchorLink>
+      </S.Navigation>
+    </S.Container>
+  );
 };
 
 export default Menu;
