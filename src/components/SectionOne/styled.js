14a1e4c915b180fb34ce0336bb87ac584b20a830
PauloMarquess <paulinho_marques2012@hotmail.com>
2021-12-28 14:29:48 -0300

    Update Readme

--- src/components/SectionOne/styled.js
+++ src/components/SectionOne/styled.js
@@ -1,30 +1,25 @@
 import styled from "styled-components";
-import Background from '../../assets/background.jpg'
-import Background2 from '../../assets/background2.jpg'
+import Background from "../../assets/background.jpg";
+import Background2 from "../../assets/background2.jpg";
 
-export const Container =styled.div`
+export const Container = styled.div`
+  background-image: url(${Background});
 
-background-image: url(${Background}) ;
+  @media (max-width: 700px) {
+    background-image: url(${Background2});
+  }
+`;
+export const H4 = styled.p`
+  display: flex;
+  color: #fff;
+  padding: 20px;
+  justify-content: end;
+  margin-right: 10%;
 
-
-@media (max-width:700px){
-    background-image:url(${Background2});
-    
-}
-
-
-`
-export const H4 =styled.p`
-
-display:flex;
-color: #FFF;
-padding: 20px;
-justify-content: end;
-margin-right: 10%;
-
-@media (max-width:700px){
+  @media (max-width: 700px) {
     padding: 20px;
     text-align: center;
-
-}
-`
+    margin: 0;
+    width: 90%;
+  }
+`;
